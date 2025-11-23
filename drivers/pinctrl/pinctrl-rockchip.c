215fd1760d3b2012438136c74955072a6a86ae31
Joey Berkovitz <joey@berkovitz.us>
2025-11-23 15:37:47 -0500

    fix compile errors

--- drivers/pinctrl/pinctrl-rockchip.c
+++ drivers/pinctrl/pinctrl-rockchip.c
@@ -829,7 +829,7 @@ static void rockchip_get_recalced_mux(struct rockchip_pin_bank *bank, int pin,
 {
 	struct rockchip_pinctrl_priv *priv = bank->priv;
 	struct rockchip_pin_ctrl *ctrl = priv->ctrl;
-	struct rockchip_mux_recalced_data *data;
+	struct rockchip_mux_recalced_data *data = NULL;
 	int i;
 
 	for (i = 0; i < ctrl->niomux_recalced; i++) {
@@ -839,7 +839,7 @@ static void rockchip_get_recalced_mux(struct rockchip_pin_bank *bank, int pin,
 			break;
 	}
 
-	if (i >= ctrl->niomux_recalced)
+	if (i >= ctrl->niomux_recalced || data == NULL)
 		return;
 
 	*reg = data->reg;
@@ -1561,7 +1561,7 @@ rockchip_get_mux_route(struct rockchip_pin_bank *bank, int pin,
 {
 	struct rockchip_pinctrl_priv *priv = bank->priv;
 	struct rockchip_pin_ctrl *ctrl = priv->ctrl;
-	struct rockchip_mux_route_data *data;
+	struct rockchip_mux_route_data *data = NULL;
 	int i;
 
 	for (i = 0; i < ctrl->niomux_routes; i++) {
@@ -1572,6 +1572,8 @@ rockchip_get_mux_route(struct rockchip_pin_bank *bank, int pin,
 	}
 
 	if (i >= ctrl->niomux_routes)
+		return ROUTE_TYPE_INVALID;
+	if (data == NULL)
 		return ROUTE_TYPE_INVALID;
 
 	*reg = data->route_offset;
