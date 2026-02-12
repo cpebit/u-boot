e31d3b495a9ea641a3fac2131f6dc84381f94bca
Chiel Prins <chiel@cpeb.it>
2026-02-12 11:07:52 +0100

    fix

--- include/configs/aaw2.h
+++ include/configs/aaw2.h
@@ -21,7 +21,7 @@
 #define ENV_MEM_LAYOUT_SETTINGS \
     "scriptaddr=0x00b00000\0" \
     "pxefile_addr_r=0x00c00000\0" \
-    "fdt_addr_r=0x00e00000\0" \
+    "fdt_addr_r=0x00d00000\0" \
     "kernel_addr_c=0x00808000\0" \
     "kernel_addr_r=0x00008000\0" \
     "ramdisk_addr_r=0x000e00000\0" \
